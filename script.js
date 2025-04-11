cedef3632020fac29d6d24f14dd1f174f0fd9531
FIIREACE22 <harshnvasoya@gmail.com>
2025-04-11 15:31:59 +0530

    Recaptcha Validations

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-function beforesubmit() {
+let captchachecked = false;
+
+function beforesubmit(event) {
+if(!captchachecked) {
     let outputdate = document.querySelector(".outputdate");
     let inputdate = document.querySelector(".inputdate");
     console.log(inputdate.value);
 
     let formatedddate = new Date(inputdate.value).toLocaleDateString("en-IN");
     outputdate.value = formatedddate;
+}else{
+    alert("Please Check Captcha First!");
+    event.preventDefault();
+}
+
 }
 function timestamp() {
     var response = document.getElementById("g-recaptcha-response");
@@ -18,3 +26,7 @@ function timestamp() {
     }
 }
 setInterval(timestamp, 500);
+
+function captchasuccess(){
+    captchachecked = true;
+}
