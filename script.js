b920351cbcc7d151a21f1d712463897e0dcd629e
FIIREACE22 <harshnvasoya@gmail.com>
2025-04-11 15:05:29 +0530

    recaptcha

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function beforesubmit(){
+function beforesubmit() {
     let outputdate = document.querySelector(".outputdate");
     let inputdate = document.querySelector(".inputdate");
     console.log(inputdate.value);
@@ -6,3 +6,15 @@ function beforesubmit(){
     let formatedddate = new Date(inputdate.value).toLocaleDateString("en-IN");
     outputdate.value = formatedddate;
 }
+function timestamp() {
+    var response = document.getElementById("g-recaptcha-response");
+    if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(
+            document.getElementsByName("captcha_settings")[0].value
+        );
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value =
+            JSON.stringify(elems);
+    }
+}
+setInterval(timestamp, 500);
